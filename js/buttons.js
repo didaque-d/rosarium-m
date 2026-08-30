cf1f844f2912e3b710ac492858731417f57d0461
didaque-d <diego.dads52@gmail.com>
2026-08-30 12:51:35 -0300

    Adicionar barra de progresso e atualizar estilos; ajustar lógica de exibição dos mistérios

--- js/buttons.js
+++ js/buttons.js
@@ -1,4 +1,5 @@
 const misterios = [
+    "Anúncio do Anjo",
     "Maria visita Isabel",
     "Nascimento de Jesus",
     "Apresentação de Jesus no templo",
@@ -17,7 +18,8 @@ const misterios = [
     "A Ascensão de Jesus",
     "A Vinda do Espírito Santo",
     "A Assunção de Maria",
-    "A Coroação de Maria"
+    "A Coroação de Maria",
+    "Rosário Concluído"
 ];
 const coroa = [
     "Mistérios Gozosos",
@@ -30,9 +32,9 @@ let quantidadeCliques = 0;
 function avancar() {
     if(quantidadeCliques < 20){
         quantidadeCliques++;
-        let avancarContador = document.getElementById("btnAvancar");
         console.log(quantidadeCliques);
         valores();
+        atualizarBarra(5);
     }
 }
 
@@ -45,77 +47,30 @@ function voltar() {
     
 }
 function valores(){
-    if(quantidadeCliques == 1){
-        document.getElementById("misterios").innerText = misterios[0];
-        document.getElementsByClassName('imagens')[0].style.right = '100%';
-        document.getElementsByClassName('ocultar')[0].style.display = "block";
-    }else if(quantidadeCliques == 2){
-        document.getElementById("misterios").innerText = misterios[1];
-        document.getElementsByClassName('imagens')[0].style.right = '200%';
-    }else if(quantidadeCliques == 3){
-        document.getElementById("misterios").innerText = misterios[2];
-        document.getElementsByClassName('imagens')[0].style.right = '300%';
-    }else if(quantidadeCliques == 4){
-        document.getElementById("misterios").innerText = misterios[3];
-        document.getElementById("coroa").innerText = coroa[0];
-        document.getElementsByClassName('imagens')[0].style.right = '400%';
-    }else if(quantidadeCliques == 5){
-        document.getElementById("misterios").innerText = misterios[4];
-        document.getElementById("coroa").innerText = coroa[1];
-        document.getElementsByClassName('imagens')[0].style.right = '500%';
-    }else if(quantidadeCliques == 6){
-        document.getElementById("misterios").innerText = misterios[5];
-        document.getElementsByClassName('imagens')[0].style.right = '600%';
-    }else if(quantidadeCliques == 7){
-        document.getElementById("misterios").innerText = misterios[6];
-        document.getElementsByClassName('imagens')[0].style.right = '700%';
-    }else if(quantidadeCliques == 8){
-        document.getElementById("misterios").innerText = misterios[7];
-        document.getElementsByClassName('imagens')[0].style.right = '800%';
-    }else if(quantidadeCliques == 9){
-        document.getElementById("misterios").innerText = misterios[8];
-        document.getElementById("coroa").innerText = coroa[1];
-        document.getElementsByClassName('imagens')[0].style.right = '900%';
-    }else if(quantidadeCliques == 10){
-        document.getElementById("misterios").innerText = misterios[9];
-        document.getElementById("coroa").innerText = coroa[2];
-        document.getElementsByClassName('imagens')[0].style.right = '1000%';
-    }else if(quantidadeCliques == 11){
-        document.getElementById("misterios").innerText = misterios[10];
-        document.getElementsByClassName('imagens')[0].style.right = '1100%';
-    }else if(quantidadeCliques == 12){
-        document.getElementById("misterios").innerText = misterios[11];
-        document.getElementsByClassName('imagens')[0].style.right = '1200%';
-    }else if(quantidadeCliques == 13){
-        document.getElementById("misterios").innerText = misterios[12];
-        document.getElementsByClassName('imagens')[0].style.right = '1300%';
-    }else if(quantidadeCliques == 14){
-        document.getElementById("misterios").innerText = misterios[13];
-        document.getElementsByClassName('imagens')[0].style.right = '1400%';
-    }else if(quantidadeCliques == 15){
-        document.getElementById("coroa").innerText = coroa[2];
-        document.getElementById("misterios").innerText = misterios[14];
-        document.getElementsByClassName('imagens')[0].style.right = '1500%';
-    }else if(quantidadeCliques == 16){
-        document.getElementById("misterios").innerText = misterios[15];
-        document.getElementById("coroa").innerText = coroa[3];
-        document.getElementsByClassName('imagens')[0].style.right = '1600%';
-    }else if(quantidadeCliques == 17){
-        document.getElementById("misterios").innerText = misterios[16];
-        document.getElementsByClassName('imagens')[0].style.right = '1700%';
-    }else if(quantidadeCliques == 18){
-        document.getElementById("misterios").innerText = misterios[17];
-        document.getElementsByClassName('imagens')[0].style.right = '1800%';
-    }else if(quantidadeCliques == 19){
-        document.getElementById("misterios").innerText = misterios[18];
-        document.getElementsByClassName('imagens')[0].style.right = '1900%';
-    }else if(quantidadeCliques == 20){
-        document.getElementById("misterios").innerText = "Rosário Concluído!";
-    }else if(quantidadeCliques == 0){
-        document.getElementById("misterios").innerText = "Anúncio do Anjo";
-        document.getElementsByClassName('imagens')[0].style.right = '0%';
+        let posicao = quantidadeCliques * 100;
+        document.getElementsByClassName('imagens')[0].style.right = posicao + '%';
+        console.log(posicao);
+        document.getElementById("misterios").innerHTML = misterios[quantidadeCliques];
+        atualizarBarra(5);
+        
+        if(quantidadeCliques < 5){
+            document.getElementById("coroa").innerHTML = coroa[0];
+        }
+        else if(quantidadeCliques >= 5 && quantidadeCliques < 10){
+            document.getElementById("coroa").innerHTML = coroa[1];
+        }
+        else if(quantidadeCliques >=10 && quantidadeCliques < 15){
+            document.getElementById("coroa").innerHTML = coroa[2];
+        }
+        else{
+            document.getElementById("coroa").innerHTML = coroa[3];
+        }
+
+        if(quantidadeCliques != 0){
+            document.getElementsByClassName("ocultar")[0].style.display = 'block';
+        }
     }
-}
+
 function retroceder(){
     quantidadeCliques = 0;
     document.getElementsByClassName('ocultar')[0].style.display = "none";
@@ -170,4 +125,17 @@ function remover(){
     }
     console.log(lista);
     console.log(index);
+}
+
+function atualizarBarra(valor){
+    valor = 5 * quantidadeCliques;
+    document.getElementById("barra-progresso").style.width = valor + "%";
+    if(quantidadeCliques == 20){
+        document.getElementById("barra").style.borderColor = "green";
+        document.getElementById("barra-progresso").style.backgroundColor = "green";
+    }
+    else{
+        document.getElementById("barra").style.borderColor = "lightskyblue";
+        document.getElementById("barra-progresso").style.backgroundColor = "lightskyblue";
+    }
 }
